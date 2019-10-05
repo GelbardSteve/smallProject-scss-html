fcde943b270d9c3c3df626341b56c7fff7d65e67
Steve Gelbard <gelbardsteve@gmail.com>
2019-10-05 19:09:25 +0300

    up

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
-const hamMenu = document.querySelector('.ham');
-const navMenu = document.querySelector('.nav-menu-close');
+const hamMenu = document.querySelector(".ham");
+const navMenu = document.querySelector(".nav-menu-close");
+const logoNavImg = document.querySelector(".logo-nav-img");
 
-hamMenu.addEventListener('click', () => {
-    navMenu.classList.toggle('nav-menu-close')
+hamMenu.addEventListener("click", () => {
+  navMenu.classList.toggle("nav-menu-close");
 });
+
+(function() {
+  logoNavImg.style.transform = "rotate(360deg)";
+  logoNavImg.style.transition = "all 1s";
+})();
